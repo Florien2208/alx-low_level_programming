bb7a6c172ba46d277242c6f330a8ee295ad4681e
Florien2208 <mahorompakanyiflorien@gmail.com>
2023-02-16 10:18:14 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include <studio.h>
+
+/**
+ * main - prints "Programming is like building a multilingual puzzle
+ * puzzle. followed by a newline.
+ * Return: Always 0.
+ */
+
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	retun 0);
+}
