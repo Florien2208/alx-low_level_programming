77b38a630d7fa732fb9ed618c015df2417dc6e8f
Florien2208 <mahorompakanyiflorien@gmail.com>
2023-02-28 03:50:37 -0800

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,7 +2,7 @@
 
 /**
  * swap_int -swap the values of two integers
- * @a: The first integer to be swaped 
+ * @a: The first integer to be swaped
  * @b: The second to be swaped
  * Return: nothing
  */
