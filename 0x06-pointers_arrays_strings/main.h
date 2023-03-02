75daa69744e7e432d4fce60f7ffe91d09f8fba27
Florien2208 <mahorompakanyiflorien@gmail.com>
2023-03-02 15:54:21 -0800

    ./0-strcat

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,4 @@
-#ifdef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
+int _putchar(char c);
 
 char *_strcat(char *dest, char *src);
 
@@ -26,4 +23,3 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 void print_buffer(char *b, int size);
-#endif
