d26b5626c06556905b015845cabb0d2d919b7dd9
Florien2208 <mahorompakanyiflorien@gmail.com>
2023-03-11 08:35:12 -0800

    0. A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - check the code for Holberton School students.
+ *
+ * @c: is an integer param
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
+
