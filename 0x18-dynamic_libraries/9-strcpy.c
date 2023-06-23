2cdef5d1f53373a390f29ff5f1b5904697de616c
Florien2208 <mahorompakanyiflorien@gmail.com>
2023-06-22 18:42:30 -0700

    smallchange

---
+++ 0x18-dynamic_libraries/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strcpy - copy a string
+* @dest: value destination
+* @src: source value
+* Return: pointer to dest
+*/
+
+char *_strcpy(char *dest, char *src)
+
+{
+int i;
+
+for (i = 0; src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+dest[i++] = '\0';
+return (dest);
+}
